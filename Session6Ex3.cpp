45964768ca3287362f4cdfdcc9d5b2e4df74e93c
dangnhat39 <nhathat111@gmail.com>
2024-11-27 13:04:12 +0700

    Session6Ex3.cpp

--- Session6Ex3.cpp
+++ Session6Ex3.cpp
@@ -1,20 +1,13 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
-    char password[22] = "16"; 
-    char input[22];              
-
+    char matkhau[20];
     printf("Nhap mat khau: ");
-    scanf("%s", input);          
-
-
-    if (strcmp(password, input) == 0) {
-        printf("Mat khau dung!\n");
-    } else {
-        printf("Mat khau sai!\n");
-    }
-
+    scanf("%s", matkhau);
+    if (matkhau[0] == '1') {
+    	printf("Mat khau dung\n"); 
+	} else {
+		printf("Mat khau sai\n");
+	} 
     return 0;
 }
-
